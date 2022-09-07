2b483e44233b1fcc8b53d5331787a22e917b1fe0
SAKSHAM <61020145+sakshamsaxena06984@users.noreply.github.com>
2022-09-07 20:11:32 +0530

    now, removing the config.xml file

---
+++ src/main/java/com/spring/jdbc/jdbcConfig.java
@@ -0,0 +1,28 @@
+package com.spring.jdbc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.stereotype.Component;
+
+@Configuration
+public class jdbcConfig {
+	@Bean("ds")
+	public DriverManagerDataSource getDataSource() {
+		DriverManagerDataSource ds=new DriverManagerDataSource();
+		ds.setDriverClassName("com.mysql.jdbc.Driver");
+		ds.setUrl("jdbc:mysql://localhost:3306/springjdbc");
+		ds.setUsername("root");
+		ds.setPassword("saks123ham@MYSQL");
+		return ds;
+	}
+	
+	@Bean("jdbcTemplate")
+	public JdbcTemplate getTemplate() {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate();
+		jdbcTemplate.setDataSource(getDataSource());
+		return jdbcTemplate;
+	}
+
+}
