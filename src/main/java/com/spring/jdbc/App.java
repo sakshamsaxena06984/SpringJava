4d75c261211d64bedc61a2d40db491779b52f1b8
SAKSHAM <61020145+sakshamsaxena06984@users.noreply.github.com>
2022-09-07 17:10:04 +0530

    first-comment

---
+++ src/main/java/com/spring/jdbc/App.java
@@ -0,0 +1,61 @@
+package com.spring.jdbc;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.spring.jdbc.dao.StudentDao;
+import com.spring.jdbc.entity.Student;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+//        System.out.println( "Hello World!" );
+    	ApplicationContext context=new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
+    	JdbcTemplate template=context.getBean("jdbcTemplate",JdbcTemplate.class);
+//    	String query="insert into student(id,name,city) values(?,?,?)";
+//    	
+//    	template.update(query,121,"Uttam","Kanpur");
+//    	System.out.println("Number of record stored!");
+//    	
+//    	String query="insert into student(id,name,city) values(?,?,?)";
+//    	template.update(query,131,"suttam","delhi");
+//    	System.out.println("Number of record stored!");
+    	
+//    	StudentDao stu=context.getBean("studentDao",StudentDao.class);
+//    	Student newstudent=new Student(78,"qqqq","pppp");
+//    	int ans_r=stu.insert(newstudent);
+//    	System.out.println("how many data is stored: "+ans_r);
+    	
+    	
+    	// now, update query
+//    	StudentDao stu=context.getBean("studentDao",StudentDao.class);
+//    	Student obj=new Student(78,"newSanju","newMumbai");
+//    	int ansR=stu.update(obj);
+//    	System.out.println("how many object update : "+ansR);
+    		
+    	//now, delete the query
+    	StudentDao stude=context.getBean("studentDao",StudentDao.class);
+//    	int ansR=stude.delete(78);
+//    	System.out.println("how many object update : "+ansR);
+    	
+    	//now, selecting the data
+//    	Student stuobj=stude.getStudent(111);
+//    	System.out.println(stuobj);
+//    	System.out.println("Id of student: "+stuobj.getId());
+//    	System.out.println("Name of student: "+stuobj.getName());
+    	
+    	//now, selecting the all data
+    	List<Student> allStudent = stude.getAllStudent();
+    	for(Student obj:allStudent) {
+    		System.out.println(obj);
+    	}
+    }
+}
---
+++ src/main/java/com/spring/jdbc/dao/StudentDaoImp.java
@@ -0,0 +1,79 @@
+package com.spring.jdbc.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.swing.tree.RowMapper;
+import javax.swing.tree.TreePath;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.spring.jdbc.entity.Student;
+
+public class StudentDaoImp implements StudentDao {
+	
+	private JdbcTemplate jdbcTemplate;
+
+	// this is the insert function initialization
+	public int insert(Student student) throws DataAccessException{
+		String query="insert into student (id,name,city) values(?,?,?)";
+	    int ans=this.jdbcTemplate.update(query,student.getId(),student.getName(),student.getCity());
+	  
+		return ans;
+	}
+	
+	// this is the update function
+	public int update(Student student) {
+		String query="update student set name=?,city=? where id=?";
+		int r = this.jdbcTemplate.update(query,student.getCity(),student.getName(),student.getId());
+		return r;
+	}
+
+	public JdbcTemplate getJdbcTemplate() {
+		return jdbcTemplate;
+	}
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	public int delete(int studentId) {
+	   String query="delete from student where id=?";
+	   int updater = jdbcTemplate.update(query,studentId);
+		return updater;
+	}
+
+	public Student getStudent(int studentId) {
+		String query="select * from student where id=?";
+//		RowMapperImpl rowMapper=new RowMapperImpl();
+//		Student student = this.jdbcTemplate.queryForObject(query, rowMapper, studentId);
+		
+		// Another way
+		Student student=this.jdbcTemplate.queryForObject(query, new org.springframework.jdbc.core.RowMapper<Student>() {
+
+			public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Student student=new Student();
+				student.setId(rs.getInt(1));
+				student.setName(rs.getString(2));
+				student.setCity(rs.getString(3));
+				return student;
+			}
+
+		
+			
+		}, studentId);
+		return student;
+	}
+
+	public List<Student> getAllStudent() {
+	    String query="select * from student";
+	    List<Student> listStudent = this.jdbcTemplate.query(query,new RowMapperImpl());
+		return listStudent;
+	}
+
+	
+
+
+}
