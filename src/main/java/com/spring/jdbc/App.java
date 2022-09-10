f7182ba58c6e56b775324a4cbcb5d8b997cca5ae
SAKSHAM <61020145+sakshamsaxena06984@users.noreply.github.com>
2022-09-10 15:48:44 +0530

    without config.xml file work is DONE

--- src/main/java/com/spring/jdbc/App.java
+++ src/main/java/com/spring/jdbc/App.java
@@ -3,6 +3,7 @@ package com.spring.jdbc;
 import java.util.List;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -18,8 +19,8 @@ public class App
     public static void main( String[] args )
     {
 //        System.out.println( "Hello World!" );
-    	ApplicationContext context=new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
-    	JdbcTemplate template=context.getBean("jdbcTemplate",JdbcTemplate.class);
+//    	ApplicationContext context=new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
+//    	JdbcTemplate template=context.getBean("jdbcTemplate",JdbcTemplate.class);
 //    	String query="insert into student(id,name,city) values(?,?,?)";
 //    	
 //    	template.update(query,121,"Uttam","Kanpur");
@@ -42,7 +43,7 @@ public class App
 //    	System.out.println("how many object update : "+ansR);
     		
     	//now, delete the query
-    	StudentDao stude=context.getBean("studentDao",StudentDao.class);
+//    	StudentDao stude=context.getBean("studentDao",StudentDao.class);
 //    	int ansR=stude.delete(78);
 //    	System.out.println("how many object update : "+ansR);
     	
@@ -53,9 +54,26 @@ public class App
 //    	System.out.println("Name of student: "+stuobj.getName());
     	
     	//now, selecting the all data
-    	List<Student> allStudent = stude.getAllStudent();
-    	for(Student obj:allStudent) {
-    		System.out.println(obj);
+//    	List<Student> allStudent = stude.getAllStudent();
+//    	for(Student obj:allStudent) {
+//    		System.out.println(obj);
+//    	}
+    	
+    	ApplicationContext context=new AnnotationConfigApplicationContext(javaConfig.class);
+    	StudentDao obj=context.getBean("studentDao",StudentDao.class);
+ /*   	
+    	//insert one record in the tables
+    	Student stu=new Student(93,"ndskja","sandkj");
+    	int insertc = obj.insert(stu);
+    	System.out.println("number of inserted records are : "+insertc);
+    	
+    	*/
+    	
+    	//fetching the whole data
+    	
+    	List<Student> lStudent=obj.getAllStudent();
+    	for(Student objs:lStudent) {
+    		System.out.println(objs);
     	}
     }
 }
--- src/main/java/com/spring/jdbc/dao/StudentDaoImp.java
+++ src/main/java/com/spring/jdbc/dao/StudentDaoImp.java
@@ -4,16 +4,19 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.swing.tree.RowMapper;
 import javax.swing.tree.TreePath;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
 import com.spring.jdbc.entity.Student;
-
+@Component("studentDao")
 public class StudentDaoImp implements StudentDao {
-	
+	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
 	// this is the insert function initialization
